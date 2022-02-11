7ac0f7160748715429ca9541ae54a248152cb58d
Caio Henrique <caiohrick@outlook.com>
2022-02-10 22:10:22 -0300

    Primeiro commit - Projeto Criado

---
+++ src/main/java/com/caiohenrique/workshopmongo/WorkshopMongoApplication.java
@@ -0,0 +1,13 @@
+package com.caiohenrique.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMongoApplication.class, args);
+	}
+
+}
