d781bcbceba39109b70c898b60feaf53bab704e7
Caio Henrique <caiohrick@outlook.com>
2022-02-13 21:28:24 -0300

    Obtendo um usuário por id

--- src/main/java/com/caiohenrique/workshopmongo/services/userService.java
+++ src/main/java/com/caiohenrique/workshopmongo/services/userService.java
@@ -1,12 +1,14 @@
 package com.caiohenrique.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.caiohenrique.workshopmongo.domain.User;
 import com.caiohenrique.workshopmongo.repository.userRepository;
+import com.caiohenrique.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class userService {
@@ -16,5 +18,10 @@ public class userService {
 
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
