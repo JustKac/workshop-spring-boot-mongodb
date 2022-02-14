e80812825b76625982dca5fae446d7722010a530
Caio Henrique <caiohrick@outlook.com>
2022-02-13 21:45:49 -0300

    Inserção de usuário com POST

--- src/main/java/com/caiohenrique/workshopmongo/services/userService.java
+++ src/main/java/com/caiohenrique/workshopmongo/services/userService.java
@@ -6,22 +6,31 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.caiohenrique.workshopmongo.DTO.UserDTO;
 import com.caiohenrique.workshopmongo.domain.User;
 import com.caiohenrique.workshopmongo.repository.userRepository;
 import com.caiohenrique.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class userService {
-	
+
 	@Autowired
 	private userRepository repo;
 
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
