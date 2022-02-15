8a67f6b00177438592c9acfac4cebf9cafe5f1ac
Caio Henrique <caiohrick@outlook.com>
2022-02-14 22:16:13 -0300

    Consulta simples com query methods

---
+++ src/main/java/com/caiohenrique/workshopmongo/resource/util/URL.java
@@ -0,0 +1,15 @@
+package com.caiohenrique.workshopmongo.resource.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class URL {
+
+	public static String dedoceParam(String text) {
+		try {
+			return URLDecoder.decode(text, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}
+}
